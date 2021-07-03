96c0b3bdb9d9fa5999ef925301ce92f0928bfca8
pctf <baranukis@gmail.com>
2021-07-03 21:08:38 +0300

    Initial commit

---
+++ src/main/java/com/github/pctf/kafkatests/KafkaTestsApplication.java
@@ -0,0 +1,13 @@
+package com.github.pctf.kafkatests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaTestsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaTestsApplication.class, args);
+    }
+
+}
